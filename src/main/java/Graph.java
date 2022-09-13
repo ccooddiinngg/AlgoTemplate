9913eb8cb1bf0862388e0b612808be395b3f6b33
HANJUN <59297616+ccooddiinngg@users.noreply.github.com>
2022-09-13 10:30:56 -0700

    init commit

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -6,32 +6,31 @@ import java.util.*;
 public class Graph {
     private static final int INF = 0x3f3f3f3f;
     public Map<Integer, List<Edge>> map;
-    public int n;
+    public int V;
 
     public Graph() {
         this.map = new HashMap<>();
-        this.n = 0;
+        this.V = 0;
     }
 
     public void build(int[][] matrix) {
-        this.n = matrix.length;
-        for (int i = 0; i < this.n; i++) {
+        this.V = matrix.length;
+        for (int i = 0; i < this.V; i++) {
             map.put(i, new ArrayList<>());
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    continue;
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (matrix[i][j] != 0) {
+                    map.get(i).add(new Edge(i, j, matrix[i][j]));
                 }
-                map.get(i).add(new Edge(i, j, matrix[i][j]));
             }
         }
     }
 
     //from 0 to n-1
     public int[] dijkstra() {
-        int[] dis = new int[n];
-        boolean[] visited = new boolean[n];
+        int[] dis = new int[V];
+        boolean[] visited = new boolean[V];
         Arrays.fill(dis, INF);
         dis[0] = 0;
         PriorityQueue<Edge> q = new PriorityQueue<>(Comparator.comparingInt(a -> a.w));
@@ -48,5 +47,28 @@ public class Graph {
             }
         }
         return dis;
+    }
+
+    public int kruskal() {
+        UnionFind unionFind = new UnionFind(V);
+
+        List<Edge> list = new ArrayList<>();
+        for (List<Edge> edges : map.values()) {
+            list.addAll(edges);
+        }
+        list.sort(Comparator.comparingInt(a -> a.w));
+        int shortestPath = 0;
+        int added = 0;
+        for (Edge edge : list) {
+            if (!unionFind.isSameSet(edge.f, edge.t)) {
+                shortestPath += edge.w;
+                added++;
+                unionFind.union(edge.f, edge.t);
+            }
+        }
+        if (added == V - 1) {
+            return shortestPath;
+        }
+        return INF;
     }
 }
--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -1,11 +1,17 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GraphTest {
     Graph graph = new Graph();
 
+    @BeforeEach
+    void setup() {
+
+    }
+
     @Test
-    void test() {
+    void testDijkstra() {
         int[][] matrix = {
                 {0, 2, 1, 0, 0, 0},
                 {2, 0, 7, 0, 8, 4},
@@ -14,8 +20,24 @@ class GraphTest {
                 {0, 8, 0, 8, 0, 5},
                 {0, 4, 3, 4, 5, 0}};
         graph.build(matrix);
-
         int[] dis = graph.dijkstra();
         Assertions.assertArrayEquals(new int[]{0, 2, 1, 8, 9, 4}, dis);
+    }
+
+    @Test
+    void testKruskal() {
+        int[][] matrix = {
+                {0, 4, 0, 0, 0, 0, 0, 8, 0},
+                {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                {0, 0, 2, 0, 0, 0, 6, 7, 0}};
+        graph.build(matrix);
+        int shortestPath = graph.kruskal();
+        Assertions.assertEquals(37, shortestPath);
     }
 }
